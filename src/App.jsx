73ed0be8ac7535c72eed9645d8464548d50117fe
MohamedAlbaali <mhmadmouaz1@gmail.com>
2025-02-17 09:01:39 +0100

    Environnement de travail

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './assets/style/App.css'
+import Header from './composents/header'
+import Footer from './composents/footer'
+import Home from './pages/home'
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
